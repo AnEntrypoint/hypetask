c15ad59b2613ac8e548d5fdc789f7cb432641e03
lanmower <almagestfraternite@gmail.com>
2023-10-02 20:42:16 +0000

    update

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-
+#! /usr/bin/env node
 const ipc = require('hyper-ipc-secure');
 require('dotenv').config();
 const runCall = require('hypeeval')
-const init = (kp, node = ipc(), serverKey = node.getSub(kp, process.env.SERVERNAME), callKey = node.getSub(kp, process.env.IPCNAME)) => {
-  node.lbserve(callKey, serverKey, process.env.IPCNAME, runCall);
-  return node;
-}
-module.exports = init
+module.exports = runCall
